10f3db1a46b9e867049459ec3f2df53a1b1c2565
Sumedh Girish <sumedh.girish@gmail.com>
2024-08-18 08:50:45 +0530

    LSP Configuration
    Configured Language-Servers using:
    1) nvim-lspconfig
    2) mason
    3) mason-lspconfig

---
+++ lua/personal/plugins/mason.lua
@@ -0,0 +1,12 @@
+
+local mason_opts = {
+  -- My options for mason
+  ui = {
+    border = "rounded",
+  },
+}
+
+return {
+  "williamboman/mason.nvim",
+  opts = mason_opts,
+}
---
+++ lua/personal/plugins/mason_lspconfig.lua
@@ -0,0 +1,22 @@
+
+local mason_lspconfig_opts = {
+  -- Options
+}
+
+local server_setup_opts = {
+  -- Server Setup Options
+}
+
+return {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = { "mason.nvim" },
+  config = function()
+    require("mason-lspconfig").setup(mason_lspconfig_opts)
+
+    require("mason-lspconfig").setup_handlers({
+      function(server_name)
+        require("lspconfig")[server_name].setup(server_setup_opts)
+      end,
+    })
+  end,
+}
