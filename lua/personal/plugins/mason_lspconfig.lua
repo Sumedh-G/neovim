7fa702d8f1603bfbbd366d0a8f831095947a3398
Sumedh Girish <sumedh.girish@gmail.com>
2024-09-04 11:52:44 +0530

    Autocompletion commit

--- lua/personal/plugins/mason_lspconfig.lua
+++ lua/personal/plugins/mason_lspconfig.lua
@@ -18,6 +18,7 @@ return {
     require("mason-lspconfig").setup_handlers({
       function(server_name)
         require("lspconfig")[server_name].setup({
+          capabilities = require('cmp_nvim_lsp').default_capabilities(),
           handlers = lspconfig_handlers,
         })
       end,
--- lua/personal/plugins/nvim-cmp.lua
+++ lua/personal/plugins/nvim-cmp.lua
@@ -1,4 +1,3 @@
-
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
@@ -12,21 +11,39 @@ return {
 
       window = {
         completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
       },
 
       mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-k>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-j>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
       }),
 
       sources = cmp.config.sources({
-        { name = 'nvim-lsp' },
+        { name = 'nvim_lsp', max_item_count=5 },
         { name = 'buffer' }
       }),
 
+    })
+
+    cmp.setup.cmdline({ '/', '?' }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' }
+      }
+    })
+
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+          { name = 'cmdline' }
+        }),
+      matching = { disallow_symbol_nonprefix_matching = false }
     })
   end
 }
